4b786e1bf5436b6d4e839c7fbef5269c39f2e3a2
dokibacsi <kvalcsi02@gmail.com>
2022-05-17 15:13:41 +0200

    masodik version

--- alapScript.js
+++ alapScript.js
@@ -9,54 +9,76 @@ const tesztKerdesekTomb = [];
 
 function betoltes(aktKerdes, key) {
     //tesztKerdesek(tesztKerdesekTomb, "tesztKerdesek.json", "altalanosIskolai", megjelenit(tesztKerdesekTomb[0]));
-    tesztKerdesek(tesztKerdesekTomb, "tesztKerdesek.json", "kozepIskolai", megjelenit(aktKerdes[key]));
+    $('.altIsk')[0].addEventListener("click", function(){
+        tesztKerdesek(tesztKerdesekTomb, "tesztKerdesek.json", "altalanosIskolai", megjelenit);
+    });
+    $('.kozepIsk')[0].addEventListener("click", function(){
+        tesztKerdesek(tesztKerdesekTomb, "tesztKerdesek.json", "kozepIskolai", megjelenit);
+    });
+    $('.fiatalFelnott')[0].addEventListener("click", function(){
+        tesztKerdesek(tesztKerdesekTomb, "tesztKerdesek.json", "fiatalFelnottKerdesek", megjelenit);
+    });
+    
+    $('.elozo')[0].addEventListener("click", elozoKerdes)
+    $('.kovetkezo')[0].addEventListener("click", kovetkezoKerdes)
 };
 
 function elozoKerdes() {
-    $('.elozo')[0].addEventListener("click", function () {
-        if (kerdesekSzamlalo < 1) {
-            kerdesekSzamlalo = 1;
-        } else {
-            kerdesekSzamlalo--;
-            megjelenit(tesztKerdesekTomb[kerdesek])
-        }
-    })
+    kerdesekSzamlalo--;
+    console.log(kerdesekSzamlalo)
+    if (kerdesekSzamlalo < 0) {
+        kerdesekSzamlalo = tesztKerdesekTomb.length-1;
+    } 
+    megjelenit()
 };
 
 function kovetkezoKerdes() {
-    $('.kovetkezo')[0].addEventListener("click", function () {
-        if (kerdesekSzamlalo > tesztKerdesekTomb.length) {
-            kerdesekSzamlalo = tesztKerdesekTomb.length;
-        } else {
-            kerdesekSzamlalo++;
-            megjelenit(tesztKerdesekTomb[kerdesek])
-        }
-    })
+    kerdesekSzamlalo++;
+    console.log(kerdesekSzamlalo)
+    if (kerdesekSzamlalo > tesztKerdesekTomb.length-1){
+        kerdesekSzamlalo = 0;
+    } 
+    megjelenit()
 };
 
 function tesztKerdesek(tomb, filenev, kulcs, cb_fgv) {
     fetch(filenev)
         .then(valasz => valasz.json())
         .then(adat => {
-            console.log(adat[kulcs])
+            //console.log(adat[kulcs])
 
             adat[kulcs].forEach(kerdesek => {
-                console.log(kerdesek),
-                    tomb.push(kerdesek)
+                //console.log(kerdesek)
+                tomb.push(kerdesek)
 
             });
-            cb_fgv();
+            cb_fgv(tomb);
         })
-        .catch(hiba => { console.log("Valami hiba történt a fájl beolvasása közben!") });
+        .catch(hiba => {
+            console.log("Valami hiba történt a fájl beolvasása közben!")
+        });
 }
 
-function megjelenit(aktKerdes) {
-    console.log(aktKerdes)
-    let szoveg = "<ul>";
-    for (var key in aktKerdes) {
-        szoveg += `<li><span>${aktKerdes[key].kerdes}, ${aktKerdes[key].valasz1}, ${aktKerdes[key].valasz2}, ${aktKerdes[key].valasz3}<span></li>`
+function megjelenit() {
+    let szoveg = "";
+    //console.log(aktKerdes)
+    szoveg += "<ul>";
+    szoveg += `<li><span>
+        <h2>${tesztKerdesekTomb[kerdesekSzamlalo].kerdes}</h2> 
+        <div>
+            <input type="radio" name="kerdes" value="." id="1">
+            <label for="m">${tesztKerdesekTomb[kerdesekSzamlalo].valasz1}</label>
+        </div>
+        <div>
+            <input type="radio" name="kerdes" value="." id="2">
+            <label for="m">${tesztKerdesekTomb[kerdesekSzamlalo].valasz2}</label>
+        </div> 
+        <div>
+            <input type="radio" name="kerdes" value="." id="3">
+            <label for="m">${tesztKerdesekTomb[kerdesekSzamlalo].valasz3}</label>
+        </div> 
+        <span></li>`
 
-    }
     szoveg += "</ul>"
     $('.teszt')[0].innerHTML = szoveg;
 }
